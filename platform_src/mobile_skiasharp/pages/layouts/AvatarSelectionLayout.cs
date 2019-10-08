ec26dc464144bf1216575fdb76c1aa9e4c4efb41
Daniel Sears <temaperacl@gmail.com>
2019-10-08 16:22:35 -0700

    Mobile: Prelude to Avatar selection

---
+++ platform_src/mobile_skiasharp/pages/layouts/AvatarSelectionLayout.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Florine;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace FlorineSkiaSharpForms
+{
+
+    public class AvatarSelectionLayout: PageLayout
+    {
+        
+        protected override void LayoutComponentWide(Grid grid, PageComponentType t, View v, int CurrentOption, int OptionCount)
+        {
+            base.LayoutComponentWide(grid, t, v, CurrentOption, OptionCount);
+        }
+
+        protected override void LayoutComponentTall(Grid grid, PageComponentType t, View v, int CurrentOption, int OptionCount)
+        {
+            switch (t)
+            {
+                case PageComponentType.Message:
+                    grid.Children.Add(v, 0, 8, 1, 10);
+                    break;
+                default:
+                    base.LayoutComponentTall(grid, t, v, CurrentOption, OptionCount);
+                    break;
+            }
+        }
+    }    
+}
